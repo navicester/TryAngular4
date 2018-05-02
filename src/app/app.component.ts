d7fe7058cdb3b874f6e7e4333df0a859c4a249e2
navicester <navicester@qq.com>
2018-05-02 19:29:01 +0800

    03 App Module & Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'srvup-root',
   templateUrl: './app.component.html',
+  //template: `{{title}} is cool`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My Apps';
 }
