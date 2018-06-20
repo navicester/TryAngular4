71e8a0dd2287eeeb819dedda16248de73ee57a44
navicester <navicester@qq.com>
2018-06-20 18:18:29 +0800

    13 -- ngx bootstrap carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppComponent } from './app.component';
 import { VideoListComponent } from './video-list/video-list.component';
@@ -9,16 +10,20 @@ import { VideoDetailComponent } from './video-detail/video-detail.component';
 
 import { AppRoutingModule} from './app.routing';
 import { SafePipe } from './utility/safe.pipe';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoListComponent,
     VideoDetailComponent,
-    SafePipe
+    SafePipe,
+    HomeComponent
   ],
   imports: [
+    //ngx-bootstraps
     BsDropdownModule.forRoot(),
+    CarouselModule.forRoot(),
     BrowserModule,
     AppRoutingModule,
   ],
