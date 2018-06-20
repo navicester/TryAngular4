4fd2807d833815262ba50a615340794b741084cf
navicester <navicester@qq.com>
2018-06-20 22:40:12 +0800

    14 -- Angular Click Events

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  // prevented = false
+
+  homeImageList = [
+    { image: "assets/images/nature/4.jpg", title:"Image 4", link:"videos/video-1"},
+    { image: "assets/images/nature/5.jpg", title:"Image 5", link:"videos/video-1"},
+    { image: "assets/images/nature/6.jpg", title:"Image 6", link:"videos/video-1"},
+  ]
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  preventNormal(event:MouseEvent, image:any){
+      if(!image.prevented){
+        console.log(image)
+        event.preventDefault()
+        // console.log(image.getAttribute("href"))
+        // image.setAttribute("href", "/videos")
+        // image.link = "/videos"
+        // image.prevented = true
+        this.router.navigate(["./videos"])
+      }
+      // alert("working ...")
   }
 
 }
